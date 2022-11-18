4df2f7661d39cbccfdae48d5a511fb9049820dc2
Stephanie Conde <stephaniecondecareer@gmail.com>
2022-11-17 20:42:01 -0800

    commit tree c2fcee3c6a6cede9f7dc5527bb84c0c1c86a4c7b

--- lib/dog.rb
+++ lib/dog.rb
@@ -1,3 +1,20 @@
 class Dog
+#setter
+  def name=(name)
+    @name = name
+  end
+
+  def breed=(breed)
+    @breed = breed
+  end
+
+#getter
+  def name
+    @name
+  end
+  
+  def breed
+    @breed
+  end
 
 end
--- lib/person.rb
+++ lib/person.rb
@@ -1,3 +1,18 @@
 class Person
+#setter
+  def name=(name)
+    @name = name
+  end
 
+  def job=(job)
+    @job = job
+  end
+#getter
+  def name
+    @name 
+  end
+
+  def job
+    @job 
+  end
 end
